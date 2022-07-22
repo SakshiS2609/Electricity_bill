0590dba6cdbb596ef1d74e78462f1db2d0580944
SakshiS2609 <sakshi2692000@gmail.com>
2022-07-22 14:08:37 +0530

    MyProject

---
+++ electricity_demo/src/main/java/com/electricity_demo/service/BillService.java
@@ -0,0 +1,17 @@
+package com.electricity_demo.service;
+
+import java.util.List;
+
+import com.electricity_demo.entity.Bill;
+import com.electricity_demo.exception.NoSuchCustomerException;
+
+public interface BillService {
+	
+	 public List<Bill> getAllBill();   
+	 public List<Bill> findById(Long billId);
+	 public Bill saveOrUpdate(Bill bill); 
+	 public int deleteBillById(Long billId);   
+	 public Bill updateBillById(Bill bill, Long billId);   
+	
+
+}
